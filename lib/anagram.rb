8485e25859b9b9e3249c56cfee47934c9a054f38
I-Am-Lain <vetter.william@gmail.com>
2020-09-21 17:57:31 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+
+require 'pry'
+class Anagram
+
+    attr_reader :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        array.select {|element| element.split("").sort == self.word.split("").sort}
+    end
+
+end
+
+listen = Anagram.new("listen")
+puts listen.match(["enlists", "google", "inlets", "banana"])
+puts listen.word
